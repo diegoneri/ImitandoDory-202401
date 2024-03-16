c2c614024e7be60e6aad735cf4bf061091ec0d9c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 22:10:14 -0300

    Resolve o exercício

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Tradutor de Baleês ---\n");
+Console.Write("Digite o texto a ser traduzido: ");
+
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("\nA Dori diria...\n");
+
+Console.WriteLine(texto
+    .Replace("a", "aaa")
+    .Replace("e", "eee")
+    .Replace("i", "iii")
+    .Replace("o", "ooo")
+    .Replace("u", "uuu")
+    .Replace("A", "AAA")
+    .Replace("E", "EEE")
+    .Replace("I", "III")
+    .Replace("O", "OOO")
+    .Replace("U", "UUU")    
+);
+Console.WriteLine();
